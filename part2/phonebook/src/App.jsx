aca7e69ad6de7cf2e5373d72d8a2b37ae836ab75
Chatree Sridaoduan <chatree.sridaoduan@gmail.com>
2025-08-17 08:29:06 +0700

    Completed part 2c with exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
@@ -6,15 +7,20 @@ import Persons from './components/Persons'
 
 const App = () => {
   // Application states
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+
+  // Effects
+  useEffect(() => {
+    console.log('use effect fired')
+    axios.get('http://localhost:3001/persons').then((response) => {
+      console.log('promised fullfilled')
+      setPersons(response.data)
+    })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   // Application handlers
   const handleNewName = (event) => {
